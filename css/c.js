a650a79c2fa0b7c54a5137965337b84ce0924b5a
Minh Huy <huydm62@wru.vn>
2018-12-06 21:48:30 +0700

    lan 2

--- css/c.js
+++ css/c.js
@@ -1,17 +1,29 @@
-jQuery(document).ready(function($) {
-    $(window).load(function() {
-     if ($('.wrapper-nav').length > 0) {
-       var _top = $('.wrapper-nav').offset().top - parseFloat($('.wrapper-nav').css('marginTop').replace(/auto/, 0));
-        $(window).scroll(function(evt) {
-         var _y = $(this).scrollTop();
-         if (_y > _top) {
-         $('.wrapper-nav').addClass('fixed');
-         $('.main-1').css("margin-top", "30px")
-         } else {
-         $('.wrapper-nav').removeClass('fixed');
-         $('.main-1').css("margin-top", "0")
-       }
-      })
-     }
-    });
-   });
+$(document).ready(function() {
+  var stt=0;
+ 
+  $("img.slide").each(function() {
+   if($(this).is(':visible')){
+    stt = $(this).attr("stt");
+   }
+  });
+  $("#next").click(function() {
+    next=++stt;
+   if(next==4){
+    stt =-1;}
+   $("img.slide").hide();
+   $("img.slide").eq(stt).show();
+  });
+ 
+ 
+  $("#prev").click(function() {
+    prev=--stt;
+   if(prev ==-1){
+    stt =4;}
+   $("img.slide").hide();
+   $("img.slide").eq(stt).show();
+  });
+  setInterval(function(){
+    $("#next").click();
+ },2000);
+ });
+
